5d0bdfde37c87818fbf44fe88b15d643ae1332e9
Ram <guptaram94@gmail.com>
2023-08-05 15:41:56 +0530

    completed resposive sidebar with angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ResponsiveSidebarMenuAngularMaterial';
+export class AppComponent
+{
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  constructor(private observer: BreakpointObserver)
+  {
+  }
+  ngAfterViewInit(): void
+  {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.observer.observe(['(max-width:800px)']).subscribe((res) =>
+    {
+      if (res.matches)
+      {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else
+      {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+
 }
